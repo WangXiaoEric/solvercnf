456e089440043af797a30b56bf81af812b90c449
WangXiaoEric <wangxiao_nick@qq.com>
2021-05-14 23:07:50 +0800

    find the getline method

--- main.cpp
+++ main.cpp
@@ -1,63 +1,82 @@
 #include <iostream>
-// #include <google/dense_hash_map>
+#include <google/dense_hash_map>
 #include "glucose.hpp"
+
+#include <fstream>
+#include <iostream>
+
 using namespace ::Glucose;
 
 int main()
 {
-    std::cout << "Test"<< std::endl;
-    Solver s;
+    char data[100];
+    std::ifstream infile;
+    infile.open("./testcase/testcase0.v");
 
-    vec<Lit> clause1;
-    clause1.push(mkLit(2));
-    clause1.push(~mkLit(4));
+    while (!infile.eof()){// find the end of the file
+        infile.getline(data,100);
+        std::cout << data << std::endl;
+    }
 
-    vec<Lit> clause2;
-    clause2.push(~mkLit(1));
-    clause2.push(~mkLit(3));
-    clause2.push(~mkLit(4));
-
-    vec<Lit> clause3;
-    clause3.push(~mkLit(3));
-    clause3.push(mkLit(4));
-
-    vec<Lit> clause4;
-    clause4.push(mkLit(1));
-    clause4.push(~mkLit(2));
-    clause4.push(~mkLit(3));
-
-    vec<Lit> clause5;
-    clause5.push(~mkLit(1));
-    clause5.push(~mkLit(3));
-    clause5.push(mkLit(4));
-
-    vec<Lit> clause6;
-    clause6.push(~mkLit(2));
-    clause6.push(~mkLit(3));
-
-    vec<Lit> clause7;
-    clause7.push(mkLit(2));
-    // clause7.push(mkLit(3));
-
-    vec<Lit> clause8;
-    clause8.push(~mkLit(2));
+ 
+	// infile >> data;
+	// std::cout << "second read data from file1.dat == " << data << std::endl;
+	infile.close();
 
 
+    // std::cout << "Test"<< std::endl;
+    // Solver s;
 
-    while(4 >= s.nVars()) 
-        s.newVar();
+    // vec<Lit> clause1;
+    // clause1.push(mkLit(2));
+    // clause1.push(~mkLit(4));
 
-    s.addClause(clause1);
-    s.addClause(clause2);
-    s.addClause(clause3);
-    s.addClause(clause4);
-    s.addClause(clause5);
-    s.addClause(clause6);
-    s.addClause(clause7);
-    s.addClause(clause7);
+    // vec<Lit> clause2;
+    // clause2.push(~mkLit(1));
+    // clause2.push(~mkLit(3));
+    // clause2.push(~mkLit(4));
+
+    // vec<Lit> clause3;
+    // clause3.push(~mkLit(3));
+    // clause3.push(mkLit(4));
+
+    // vec<Lit> clause4;
+    // clause4.push(mkLit(1));
+    // clause4.push(~mkLit(2));
+    // clause4.push(~mkLit(3));
+
+    // vec<Lit> clause5;
+    // clause5.push(~mkLit(1));
+    // clause5.push(~mkLit(3));
+    // clause5.push(mkLit(4));
+
+    // vec<Lit> clause6;
+    // clause6.push(~mkLit(2));
+    // clause6.push(~mkLit(3));
+
+    // vec<Lit> clause7;
+    // clause7.push(mkLit(2));
+    // // clause7.push(mkLit(3));
+
+    // vec<Lit> clause8;
+    // clause8.push(~mkLit(2));
 
 
-    bool ans = s.solve();
-    std::cout << "This is :" <<ans << std::endl;
+
+    // while(4 >= s.nVars()) 
+    //     s.newVar();
+
+    // s.addClause(clause1);
+    // s.addClause(clause2);
+    // s.addClause(clause3);
+    // s.addClause(clause4);
+    // s.addClause(clause5);
+    // s.addClause(clause6);
+    // s.addClause(clause7);
+    // s.addClause(clause7);
+
+
+    // bool ans = s.solve();
+    // std::cout << "This is :" <<ans << std::endl;
     return 0;
 }

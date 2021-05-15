dc6cb0cad76abb51cb9e4cf38b7147f35bc98d96
WangXiaoEric <wangxiao_nick@qq.com>
2021-05-16 02:00:34 +0800

    deal all cases.

--- main.cpp
+++ main.cpp
@@ -298,6 +298,73 @@ int main()
 
         }
 
+        // deal with buf
+        if(data.find("buf ") != string::npos){
+            int y = 0;
+            int x = 0;
+            int parameterCount = 1;
+            bool isBeforeCommenStr = false;
+            int lastCharIndex = 0;
+            for (int i = 0; i < data.length(); i++)
+            {
+                if(' ' == data[i] || ',' == data[i] || ';' == data[i] || '(' == data[i] || ')' == data[i])
+                    {
+                        
+                        //then should store one parameter
+                        if(isBeforeCommenStr){
+                            result = new char [i - lastCharIndex + 1];
+                            //resultWire = new char [i - lastCharIndex + 1];
+                            getStringFromCharArrayAdvanced(data, lastCharIndex, i, result);
+                            // string tempSt(result);
+                            // string tempSt = result;
+                            if(strcmp("buf", result) == 0){
+                                isBeforeCommenStr = false;
+                                continue;
+                            }
+                            std::cout << "key  " << result <<  " value -> " << varibleMap[result] << std::endl;                            
+                            if(parameterCount == 1){
+                                y = varibleMap[result];
+                                parameterCount++;
+                            }
+                            else if (parameterCount == 2)
+                            {
+                                x = varibleMap[result];
+                                parameterCount++;
+                            }
+                            
+                            isBeforeCommenStr = false;
+                            //delete result;
+                        }else{
+                            //string deal;
+                            continue;
+                        }
+                        
+                    }
+                    // If read the real characteristic
+                    else
+                    {
+                        if(!isBeforeCommenStr){
+                            isBeforeCommenStr =true;
+                            lastCharIndex = i;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+            }
+            //(-y+x)(y + -x )
+            vec<Lit> clause;
+            clause.push(~mkLit(y));
+            clause.push(mkLit(x));
+            s.addClause(clause);
+
+            vec<Lit> clause1;
+            clause1.push(mkLit(y));
+            clause1.push(~mkLit(x));
+            s.addClause(clause1);
+
+        }
 
         // deal with or  (y+-x1)(y+-r2)(-y +x1 + x2)
         if(data.find("or ") != string::npos){
@@ -377,12 +444,412 @@ int main()
             s.addClause(clause2);
         }
 
+        // deal with and  (-y+x1)(-y+x2)(y+-x1+-x2)
+        if(data.find("and ") != string::npos){
+            int y = 0;
+            int x1 = 0;
+            int x2 = 0;
+            int parameterCount = 1;
+            bool isBeforeCommenStr = false;
+            int lastCharIndex = 0;
+            for (int i = 0; i < data.length(); i++)
+            {
+                if(' ' == data[i] || ',' == data[i] || ';' == data[i] || '(' == data[i] || ')' == data[i])
+                    {
+                        
+                        //then should store one parameter
+                        if(isBeforeCommenStr){
+                            result = new char [i - lastCharIndex + 1];
+                            //resultWire = new char [i - lastCharIndex + 1];
+                            getStringFromCharArrayAdvanced(data, lastCharIndex, i, result);
+                            // string tempSt(result);
+                            // string tempSt = result;
+                            if(strcmp("and", result) == 0){
+                                isBeforeCommenStr = false;
+                                continue;
+                            }
+                            std::cout << "key  " << result <<  " value -> " << varibleMap[result] << std::endl;                            
+                            if(parameterCount == 1){
+                                y = varibleMap[result];
+                                parameterCount++;
+                            }
+                            else if (parameterCount == 2)
+                            {
+                                x1 = varibleMap[result];
+                                parameterCount++;
+                            }
+                            else if (parameterCount == 3)
+                            {
+                                x2 = varibleMap[result];
+                                parameterCount++;
+                            }
+                            
+                            isBeforeCommenStr = false;
+                            //delete result;
+                        }else{
+                            //string deal;
+                            continue;
+                        }
+                    }
+                    // If read the real characteristic
+                    else
+                    {
+                        if(!isBeforeCommenStr){
+                            isBeforeCommenStr =true;
+                            lastCharIndex = i;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+            }
+            // deal with and  (-y+x1)(-y+x2)(y+-x1+-x2)
+            vec<Lit> clause;
+            clause.push(~mkLit(y));
+            clause.push(mkLit(x1));
+            s.addClause(clause);
 
+            vec<Lit> clause1;
+            clause1.push(~mkLit(y));
+            clause1.push(mkLit(x2));
+            s.addClause(clause1);
 
+            vec<Lit> clause2;
+            clause2.push(mkLit(y));
+            clause2.push(~mkLit(x1));
+            clause2.push(~mkLit(x2));
+            s.addClause(clause2);
+        }
 
+        // deal nand (y+x1)(y+x2)(-y+-x1 +-x2)
+        if(data.find("nand ") != string::npos){
+            int y = 0;
+            int x1 = 0;
+            int x2 = 0;
+            int parameterCount = 1;
+            bool isBeforeCommenStr = false;
+            int lastCharIndex = 0;
+            for (int i = 0; i < data.length(); i++)
+            {
+                if(' ' == data[i] || ',' == data[i] || ';' == data[i] || '(' == data[i] || ')' == data[i])
+                    {
+                        
+                        //then should store one parameter
+                        if(isBeforeCommenStr){
+                            result = new char [i - lastCharIndex + 1];
+                            //resultWire = new char [i - lastCharIndex + 1];
+                            getStringFromCharArrayAdvanced(data, lastCharIndex, i, result);
+                            // string tempSt(result);
+                            // string tempSt = result;
+                            if(strcmp("nand", result) == 0){
+                                isBeforeCommenStr = false;
+                                continue;
+                            }
+                            std::cout << "key  " << result <<  " value -> " << varibleMap[result] << std::endl;                            
+                            if(parameterCount == 1){
+                                y = varibleMap[result];
+                                parameterCount++;
+                            }
+                            else if (parameterCount == 2)
+                            {
+                                x1 = varibleMap[result];
+                                parameterCount++;
+                            }
+                            else if (parameterCount == 3)
+                            {
+                                x2 = varibleMap[result];
+                                parameterCount++;
+                            }
+                            
+                            isBeforeCommenStr = false;
+                            //delete result;
+                        }else{
+                            //string deal;
+                            continue;
+                        }
+                    }
+                    // If read the real characteristic
+                    else
+                    {
+                        if(!isBeforeCommenStr){
+                            isBeforeCommenStr =true;
+                            lastCharIndex = i;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+            }
+            //deal nand (y+x1)(y+x2)(-y+-x1 +-x2)
+            vec<Lit> clause;
+            clause.push(mkLit(y));
+            clause.push(mkLit(x1));
+            s.addClause(clause);
 
+            vec<Lit> clause1;
+            clause1.push(mkLit(y));
+            clause1.push(mkLit(x2));
+            s.addClause(clause1);
 
-        std::cout << data << std::endl;
+            vec<Lit> clause2;
+            clause2.push(~mkLit(y));
+            clause2.push(~mkLit(x1));
+            clause2.push(~mkLit(x2));
+            s.addClause(clause2);
+        }
+
+        // deal nor (-y+-x1))(-y+-x2)(y +x1 +x2)
+        if(data.find("nor ") != string::npos){
+            int y = 0;
+            int x1 = 0;
+            int x2 = 0;
+            int parameterCount = 1;
+            bool isBeforeCommenStr = false;
+            int lastCharIndex = 0;
+            for (int i = 0; i < data.length(); i++)
+            {
+                if(' ' == data[i] || ',' == data[i] || ';' == data[i] || '(' == data[i] || ')' == data[i])
+                    {
+                        
+                        //then should store one parameter
+                        if(isBeforeCommenStr){
+                            result = new char [i - lastCharIndex + 1];
+                            //resultWire = new char [i - lastCharIndex + 1];
+                            getStringFromCharArrayAdvanced(data, lastCharIndex, i, result);
+                            // string tempSt(result);
+                            // string tempSt = result;
+                            if(strcmp("nor", result) == 0){
+                                isBeforeCommenStr = false;
+                                continue;
+                            }
+                            std::cout << "key  " << result <<  " value -> " << varibleMap[result] << std::endl;                            
+                            if(parameterCount == 1){
+                                y = varibleMap[result];
+                                parameterCount++;
+                            }
+                            else if (parameterCount == 2)
+                            {
+                                x1 = varibleMap[result];
+                                parameterCount++;
+                            }
+                            else if (parameterCount == 3)
+                            {
+                                x2 = varibleMap[result];
+                                parameterCount++;
+                            }
+                            
+                            isBeforeCommenStr = false;
+                            //delete result;
+                        }else{
+                            //string deal;
+                            continue;
+                        }
+                    }
+                    // If read the real characteristic
+                    else
+                    {
+                        if(!isBeforeCommenStr){
+                            isBeforeCommenStr =true;
+                            lastCharIndex = i;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+            }
+            //deal nor (-y+-x1))(-y+-x2)(y +x1 +x2)
+            vec<Lit> clause;
+            clause.push(~mkLit(y));
+            clause.push(mkLit(x1));
+            s.addClause(clause);
+
+            vec<Lit> clause1;
+            clause1.push(~mkLit(y));
+            clause1.push(mkLit(x2));
+            s.addClause(clause1);
+
+            vec<Lit> clause2;
+            clause2.push(mkLit(y));
+            clause2.push(mkLit(x1));
+            clause2.push(mkLit(x2));
+            s.addClause(clause2);
+        }
+
+        // deal xor (-y+x1+x2)(-y+-x1+-x2)(y+-x1+x2)(y +x1+-x2)
+        if(data.find("xor ") != string::npos){
+            int y = 0;
+            int x1 = 0;
+            int x2 = 0;
+            int parameterCount = 1;
+            bool isBeforeCommenStr = false;
+            int lastCharIndex = 0;
+            for (int i = 0; i < data.length(); i++)
+            {
+                if(' ' == data[i] || ',' == data[i] || ';' == data[i] || '(' == data[i] || ')' == data[i])
+                    {
+                        
+                        //then should store one parameter
+                        if(isBeforeCommenStr){
+                            result = new char [i - lastCharIndex + 1];
+                            //resultWire = new char [i - lastCharIndex + 1];
+                            getStringFromCharArrayAdvanced(data, lastCharIndex, i, result);
+                            // string tempSt(result);
+                            // string tempSt = result;
+                            if(strcmp("xor", result) == 0){
+                                isBeforeCommenStr = false;
+                                continue;
+                            }
+                            std::cout << "key  " << result <<  " value -> " << varibleMap[result] << std::endl;                            
+                            if(parameterCount == 1){
+                                y = varibleMap[result];
+                                parameterCount++;
+                            }
+                            else if (parameterCount == 2)
+                            {
+                                x1 = varibleMap[result];
+                                parameterCount++;
+                            }
+                            else if (parameterCount == 3)
+                            {
+                                x2 = varibleMap[result];
+                                parameterCount++;
+                            }
+                            
+                            isBeforeCommenStr = false;
+                            //delete result;
+                        }else{
+                            //string deal;
+                            continue;
+                        }
+                    }
+                    // If read the real characteristic
+                    else
+                    {
+                        if(!isBeforeCommenStr){
+                            isBeforeCommenStr =true;
+                            lastCharIndex = i;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+            }
+            //deal xor (-y+x1+x2)(-y+-x1+-x2)(y+-x1+x2)(y +x1+-x2)
+            vec<Lit> clause;
+            clause.push(~mkLit(y));
+            clause.push(mkLit(x1));
+            clause.push(mkLit(x2));
+            s.addClause(clause);
+
+            vec<Lit> clause1;
+            clause1.push(~mkLit(y));
+            clause1.push(~mkLit(x1));
+            clause1.push(~mkLit(x2));
+            s.addClause(clause1);
+
+            vec<Lit> clause2;
+            clause2.push(mkLit(y));
+            clause2.push(~mkLit(x1));
+            clause2.push(mkLit(x2));
+            s.addClause(clause2);
+
+            vec<Lit> clause3;
+            clause3.push(mkLit(y));
+            clause3.push(mkLit(x1));
+            clause3.push(~mkLit(x2));
+            s.addClause(clause3);
+        }
+
+        // deal xnor (y+x1+x2)(y +-x1 +-x2)(-y+-x1+x2)(-y +x1 +-x2)
+        if(data.find("xnor ") != string::npos){
+            int y = 0;
+            int x1 = 0;
+            int x2 = 0;
+            int parameterCount = 1;
+            bool isBeforeCommenStr = false;
+            int lastCharIndex = 0;
+            for (int i = 0; i < data.length(); i++)
+            {
+                if(' ' == data[i] || ',' == data[i] || ';' == data[i] || '(' == data[i] || ')' == data[i])
+                    {
+                        
+                        //then should store one parameter
+                        if(isBeforeCommenStr){
+                            result = new char [i - lastCharIndex + 1];
+                            //resultWire = new char [i - lastCharIndex + 1];
+                            getStringFromCharArrayAdvanced(data, lastCharIndex, i, result);
+                            // string tempSt(result);
+                            // string tempSt = result;
+                            if(strcmp("xnor", result) == 0){
+                                isBeforeCommenStr = false;
+                                continue;
+                            }
+                            std::cout << "key  " << result <<  " value -> " << varibleMap[result] << std::endl;                            
+                            if(parameterCount == 1){
+                                y = varibleMap[result];
+                                parameterCount++;
+                            }
+                            else if (parameterCount == 2)
+                            {
+                                x1 = varibleMap[result];
+                                parameterCount++;
+                            }
+                            else if (parameterCount == 3)
+                            {
+                                x2 = varibleMap[result];
+                                parameterCount++;
+                            }
+                            
+                            isBeforeCommenStr = false;
+                            //delete result;
+                        }else{
+                            //string deal;
+                            continue;
+                        }
+                    }
+                    // If read the real characteristic
+                    else
+                    {
+                        if(!isBeforeCommenStr){
+                            isBeforeCommenStr =true;
+                            lastCharIndex = i;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+            }
+            //deal xor (y+x1+x2)(y +-x1 +-x2)(-y+-x1+x2)(-y +x1 +-x2)
+            vec<Lit> clause;
+            clause.push(mkLit(y));
+            clause.push(mkLit(x1));
+            clause.push(mkLit(x2));
+            s.addClause(clause);
+
+            vec<Lit> clause1;
+            clause1.push(mkLit(y));
+            clause1.push(~mkLit(x1));
+            clause1.push(~mkLit(x2));
+            s.addClause(clause1);
+
+            vec<Lit> clause2;
+            clause2.push(~mkLit(y));
+            clause2.push(~mkLit(x1));
+            clause2.push(mkLit(x2));
+            s.addClause(clause2);
+
+            vec<Lit> clause3;
+            clause3.push(~mkLit(y));
+            clause3.push(mkLit(x1));
+            clause3.push(~mkLit(x2));
+            s.addClause(clause3);
+        }
+
     }
 
 
